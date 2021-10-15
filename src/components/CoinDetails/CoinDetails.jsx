8fd53e88ef1d5afee5f6632e57fa4e13bc3fde9a
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-10-15 01:06:36 -0500

    coin details are aflowing

--- src/components/CoinDetails/CoinDetails.jsx
+++ src/components/CoinDetails/CoinDetails.jsx
@@ -19,7 +19,18 @@ function CoinDetails({ card }) {
     const history = useHistory();
     const dispatch = useDispatch();
     const [chartData, setChartData] = useState([]);
-    const [coinDetails, setCoinDetails] = useState([]);
+    const [isLoading, setIsLoading] = useState(false); //initial value false on page loading
+
+    const [coinInfo, setCoinInfo] = useState([]); //all info for coin
+    const [coinName, setCoinName] = useState('');
+    const [coinSymbol, setCoinSymbol] = useState('');
+    const [coinPlatform, setCoinPlatform] = useState('');
+    const [coinTwitter, setCoinTwitter] = useState('');
+    const [coinWebsite, setCoinWebsite] = useState('');
+    const [coinForum, setCoinForum] = useState('');
+    const [coinDescription, setCoinDescription] = useState('');
+
+
 
     const navBack = () => {
         history.push('/search')
@@ -38,46 +49,59 @@ function CoinDetails({ card }) {
      * I hope this will suffice!
      * 
      * This message will self destruct 
-     */           //todo needs own page
+     * 
+     */           //todo needs own page = chartPage
+    useEffect(() => {
+        setIsLoading(true)
+        axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7&interval=hourly`)
+            .then(res => {
+                setChartData(res.data.prices);
+                console.log('Response of seven day chart data is:', res.data.prices);
+            }).catch(error => console.log('error getting cryptos:', error))
+        fetchCoinInfo()
+    }, [])
+
+    //todo needs own page = infoPage
+    const fetchCoinInfo = async () => {
+        console.log('in fetchCoininfo! id:', id)
+        await axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
+            .then(res => {
+                setCoinInfo(res.data);
+                console.log('res.data in fetchCoinInfo is:', res.data)
+                setCoinName(res.data.name)
+                setCoinSymbol(res.data.symbol.toUpperCase())
+                // setCoinPlatform(res.data.asset_platform_id)  // not cooperating
+                setCoinTwitter(res.data.links.twitter_screen_name)
+                setCoinWebsite(res.data.links.homepage)
+                setCoinForum(res.data.links.official_forum_url)
+                setCoinDescription(res.data.description.en)
+                setIsLoading(false);
+
+            }).catch(error => console.log('error getting coin details!', error))
+
+    }
+
+
+    //optimize all in one - probably //deletelater
     // useEffect(() => {
     //     axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7&interval=hourly`)
     //         .then(res => {
     //             setChartData(res.data.prices);
     //             console.log('Response of seven day chart data is:', res.data.prices);
     //         }).catch(error => console.log('error getting cryptos:', error))
-    //     fetchCoinInfo()
-    // }, [])
-
-    //     //todo needs own page
-    // const fetchCoinInfo = async () => {
-    //     console.log('in fetchCoininfo! id:', id)
-    //     await axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
+    //     axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
     //         .then(res => {
     //             setCoinDetails(res.data);
     //             console.log('res.data in fetchCoinInfo is:', res.data)
     //         }).catch(error => console.log('error getting coin details!', error))
 
-    // }
-
-    useEffect(() => {
-        axios.get(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=7&interval=hourly`)
-            .then(res => {
-                setChartData(res.data.prices);
-                console.log('Response of seven day chart data is:', res.data.prices);
-            }).catch(error => console.log('error getting cryptos:', error))
-        axios.get(`https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`)
-            .then(res => {
-                setCoinDetails(res.data);
-                console.log('res.data in fetchCoinInfo is:', res.data)
-            }).catch(error => console.log('error getting coin details!', error))
-
-    }, [])
+    // }, [])
 
 
 
 
-    const renderPage = () => { //FIX THIS
-        if (chartData && coinDetails) {
+    const renderPage = () => { //Shows a loading dialogue if loading (CHANGE TO A COOL ANIMATION)
+        if (isLoading) {
             return <div>Loading coin info...</div>
         }
     }
@@ -87,20 +111,20 @@ function CoinDetails({ card }) {
     return (
         <Container className={classes.detailsPage}>
             {/* <Typography className={classes.pageHeader} variant="h3">Coin Details Page</Typography> */}
-            <Typography variant="h4">{coinDetails.name}</Typography>
-            {/* <Typography variant="h2">{coinDetails.symbol.toUpperCase()}</Typography> */}
-            <Typography variant="h5">{coinDetails.categories}</Typography><br />
-
-            {/* <Typography variant="h5">Platform: {coinDetails.asset_platform_id}</Typography><br /> */}
-
-            {/* <Typography variant="h5">{coinDetails.description.en}</Typography><br /> */}
-{/* 
-            <Typography variant="h5">Twitter: {coinDetails.links.twitter_screen_name}</Typography><br />
-            <Typography variant="h5">Website: {coinDetails.links.homepage}</Typography><br /> */}
+            <Typography variant="h4">{coinName}</Typography>
+            <Typography variant="h3">{coinSymbol}</Typography>
+            {/* {coinPlatform == null || coinPlatform == undefined ? (<Typography variant="h5">Platform: N/A</Typography>) : (<Typography variant="h5">{coinPlatform}</Typography>)} */}
+            <Typography variant="h5" style={{ color: 'blue' }}>@{coinTwitter}</Typography>
+            <Typography variant="h5" style={{ color: 'red' }}>{coinWebsite}</Typography>
+            <Typography variant="h5" style={{ color: 'purple' }}>{coinForum}</Typography>
+            <Typography variant="h6">{coinDescription}</Typography><br />
 
 
+            {/* <Typography variant="h5">Website: {coinInfo.links.homepage}</Typography><br /> */}
 
-            {JSON.stringify(coinDetails)}
+
+
+            {JSON.stringify(coinInfo)}
             {JSON.stringify(chartData)}
             <Button className={classes.loginButton} size="large" variant="contained" onClick={() => navBack()}>Go Back</Button>
             <Container>
