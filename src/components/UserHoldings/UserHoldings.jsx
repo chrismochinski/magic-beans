20e4b6a4b43c9f1009f641aefd28b45c36d4b48d
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-10-17 15:07:37 -0500

    number / decimal cleanup

--- src/components/UserHoldings/UserHoldings.jsx
+++ src/components/UserHoldings/UserHoldings.jsx
@@ -23,16 +23,30 @@ const UserHoldings = () => {
         dispatch({ type: 'FETCH_USER_HOLDINGS', payload: store.user.id });
     }, [])
 
-    function getCurrentCryptoValue(data, id, count) {
+
+    //function to get total value per coin
+    const getCurrentCryptoValue = (data, id, count) => {
         let target = data.filter(function (data) {
             return (data['id'] == id);
         });
-        if (0 < target.length) {
+        if (target.length > 0) {
             return (count * target[0]['current_price']).toFixed(2);
         } else {
             return 'N/A';
         }
     }
+
+    //function to clean up held coins decimal chaos
+    const fixHeldDecimals = (coinAmount) => {
+        let value = Number(coinAmount);
+        let res = coinAmount.split('.');
+        if(res.lentgh == 1 || res[1].length < 3) {
+            value = value.toFixed(2)
+        }
+        return value;
+    }
+
+    
 
 
     return (
@@ -45,7 +59,7 @@ const UserHoldings = () => {
                         return (
                             
                                 <li key={holding.id}>
-                                    <Typography className='userAssetTypography' >{holding.symbol} - {holding.coins_held} - {getCurrentCryptoValue(cryptoList, holding.coin_id, holding.coins_held)}</Typography>
+                                    <Typography className='userAssetTypography' >{holding.symbol} - {fixHeldDecimals(holding.coins_held)} - {getCurrentCryptoValue(cryptoList, holding.coin_id, holding.coins_held)}</Typography>
                                 </li>
                          
                         )
@@ -56,6 +70,5 @@ const UserHoldings = () => {
         </div>
     )
 }
-
 
 export default UserHoldings
