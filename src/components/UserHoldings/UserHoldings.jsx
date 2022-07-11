8bea0861890d1e29d5147041fe0d446b26d83053
Chris Mochinski <cmochinski@gmail.com>
2022-07-11 15:44:27 -0500

    remove comments on UserHoldings, cleanup code w/ prettier

--- src/components/UserHoldings/UserHoldings.jsx
+++ src/components/UserHoldings/UserHoldings.jsx
@@ -61,7 +61,6 @@ const UserHoldings = () => {
     Swal.fire({
       title: `Remove all ${name}?`,
       text: "This position will be removed.",
-      // icon: 'warning',
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#9333F0",
@@ -87,7 +86,7 @@ const UserHoldings = () => {
   };
 
   const handleAddClick = () => {
-    history.push("/search"); //important add this page for search, SHARE WITH HOURGLASS HAMBURGER LINK
+    history.push("/search");
   };
 
   //function to get total value per coin
@@ -98,10 +97,10 @@ const UserHoldings = () => {
     });
     if (target.length > 0) {
       totalToDisplay(count * target[0]["current_price"]); //send to GRAND TOTAL function
-      finalValue = (count * target[0]["current_price"]).toLocaleString(
-        undefined,
-        { minimumFractionDigits: 2, maximumFractionDigits: 2 }
-      ); //format price display
+      finalValue = (count * target[0]["current_price"]).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      }); //format price display
     } else {
       finalValue = "N/A"; //show 'N/A' on any given holding if price unavailable
     }
@@ -115,8 +114,7 @@ const UserHoldings = () => {
     } else {
       runningTotalArray.push(incomingNumber);
     }
-    const reducer = (previousValue, currentValue) =>
-      previousValue + currentValue;
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
     totalToSend = runningTotalArray.reduce(reducer);
 
     return <div>{totalToSend}</div>;
@@ -131,11 +129,7 @@ const UserHoldings = () => {
   return (
     <div>
       {holdings.length === 0 ? (
-        <Typography
-          className={classes.assetHeadline}
-          variant="h4"
-          style={{ color: "#F70C8A" }}
-        >
+        <Typography className={classes.assetHeadline} variant="h4" style={{ color: "#F70C8A" }}>
           No Assets Yet
         </Typography>
       ) : (
@@ -143,64 +137,36 @@ const UserHoldings = () => {
           <Table>
             <TableHead>
               <TableRow className={classes.userTableHeader}>
-                <TableCell
-                  style={{ padding: "2px" }}
-                  className={classes.userTableHeaderCell}
-                >
+                <TableCell style={{ padding: "2px" }} className={classes.userTableHeaderCell}>
                   Coin
                 </TableCell>
-                <TableCell
-                  style={{ padding: "2px" }}
-                  className={classes.userTableHeaderCell}
-                >
+                <TableCell style={{ padding: "2px" }} className={classes.userTableHeaderCell}>
                   Held
                 </TableCell>
-                <TableCell
-                  style={{ padding: "2px" }}
-                  className={classes.userTableHeaderCell}
-                >
+                <TableCell style={{ padding: "2px" }} className={classes.userTableHeaderCell}>
                   Value
                 </TableCell>
-               
               </TableRow>
             </TableHead>
             <TableBody className={classes.holdingsTableBody}>
               {holdings.map((holding) => {
                 return (
                   <TableRow key={holding.id}>
-                    <TableCell
-                      style={{ padding: "3px" }}
-                      className={classes.holdingSymbol}
-                    >
+                    <TableCell style={{ padding: "3px" }} className={classes.holdingSymbol}>
                       {holding.symbol}
                     </TableCell>
-                    <TableCell
-                      style={{ padding: "3px" }}
-                      className={classes.holdingAmount}
-                    >
+                    <TableCell style={{ padding: "3px" }} className={classes.holdingAmount}>
                       {fixHeldDecimals(holding.coins_held)}
                     </TableCell>
-                    <TableCell
-                      style={{ padding: "3px" }}
-                      className={classes.holdingVal}
-                    >
-                      $
-                      {getCurrentCryptoValue(
-                        cryptoList,
-                        holding.coin_id,
-                        holding.coins_held
-                      )}
+                    <TableCell style={{ padding: "3px" }} className={classes.holdingVal}>
+                      ${getCurrentCryptoValue(cryptoList, holding.coin_id, holding.coins_held)}
                     </TableCell>
-                    <TableCell
-                      style={{ padding: "3px" }}
-                      className={classes.holdingDelete}
-                    >
+                    <TableCell style={{ padding: "3px" }} className={classes.holdingDelete}>
                       <IconButton
                         variant="contained"
                         size="small"
                         className={classes.holdingsDeleteButton}
-                        onClick={() => deleteWarn(holding.id, holding.name)}
-                      >
+                        onClick={() => deleteWarn(holding.id, holding.name)}>
                         <DeleteForeverIcon
                           style={{
                             color: "#9A0D01",
@@ -210,22 +176,12 @@ const UserHoldings = () => {
                         />
                       </IconButton>
                     </TableCell>
-                    <TableCell
-                      style={{ padding: "3px" }}
-                      className={classes.holdingModify}
-                    >
+                    <TableCell style={{ padding: "3px" }} className={classes.holdingModify}>
                       <IconButton
                         variant="contained"
                         size="small"
                         className={classes.holdingsModifyButton}
-                        onClick={() =>
-                          handleModifyPress(
-                            holding.name,
-                            holding.id,
-                            holding.coins_held
-                          )
-                        }
-                      >
+                        onClick={() => handleModifyPress(holding.name, holding.id, holding.coins_held)}>
                         <ImportExportIcon
                           style={{
                             color: "#D4C215",
@@ -251,12 +207,10 @@ const UserHoldings = () => {
           textAlign: "center",
           fontFamily: "Poppins",
         }}
-        variant="h4"
-      >
+        variant="h4">
         Total:{" "}
         <b>
-          {/* User grand total is HERE: */}
-          $
+          {/* User grand total is HERE: */}$
           {totalToSend.toLocaleString(undefined, {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2,
@@ -276,8 +230,7 @@ const UserHoldings = () => {
               fontWeight: "bolder",
               backgroundColor: "#5C9827",
               color: "white",
-            }}
-          >
+            }}>
             <b>Add</b>
           </Button>
         </Grid>
