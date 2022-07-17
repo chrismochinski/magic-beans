7b7c8669a91687dc9375195c3b38fe358d18163c
Chris Mochinski <cmochinski@gmail.com>
2022-07-16 20:36:08 -0500

    template class remove unneeded comments

--- src/components/TemplateClass/TemplateClass.jsx
+++ src/components/TemplateClass/TemplateClass.jsx
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-// Basic class component structure for React with default state
-// value setup. When making a new component be sure to replace
-// the component name TemplateClass with the name for the new
-// component.
-
-// YOU CANNOT USE HOOKS IN CLASS COMPONENTS -- usually Higher Order Components are used instead
-// Class Components have different syntax, including `this`
-
 class TemplateClass extends Component {
   state = {
     heading: "Class Component",
